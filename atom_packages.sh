d47d821c18001a8e34d7ae5b4b744f5518744df1
Alejandro Bezdjian <alebezdjian@gmail.com>
2016-10-07 22:20:50 -0300

    Update atom_packages.sh

--- atom_packages.sh
+++ atom_packages.sh
@@ -2,21 +2,20 @@
 
 function install_atom_packages {
   apm install travis-ci-status
-  apm install merge-conflicts
+  apm install linter
+  apm install linter-rubocop
   apm install minimap
   apm install file-icons
   apm install highlight-selected
   apm install ruby-slim
-  apm install rubocop-auto-correct
   apm install rails-i18n-autocomplete
-  apm install rails-partials
   apm install color-picker
   apm install multi-cursor
-  apm install pretty-json
   apm install atom-prettify
   apm install rspec
   apm install goto-definition
   apm install language-elixir
+  apm install labguage-rust
 }
 
 install_atom_packages
